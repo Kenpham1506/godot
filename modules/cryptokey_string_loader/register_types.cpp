f5e3b28f1db3c88061d04b9317e4330ff3c1b95c
Kenpham1506 <165395073+Kenpham1506@users.noreply.github.com>
2025-04-15 17:35:13 +0700

    Update register_types.cpp

--- modules/cryptokey_string_loader/register_types.cpp
+++ modules/cryptokey_string_loader/register_types.cpp
@@ -7,7 +7,7 @@ void initialize_cryptokey_string_loader_module(ModuleInitializationLevel p_level
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
 		return;
 
-	ClassDB::bind_method(D_METHOD("load_from_string", "pem_string"), &CryptoKeyString::load_from_string);
+	ClassDB::bind_method(D_METHOD("load_key_from_string", "pem_string"), &CryptoKeyString::load_key_from_string);
 }
 
 void uninitialize_cryptokey_string_loader_module(ModuleInitializationLevel p_level) {
