6f575ce848fffe0d02924079807c1965b63903f0
Kenpham1506 <165395073+Kenpham1506@users.noreply.github.com>
2025-04-15 17:20:09 +0700

    Create register_types.cpp

---
+++ modules/cryptokey_string_loader/register_types.cpp
@@ -0,0 +1,16 @@
+#include "register_types.h"
+#include "crypto_key_string.h"
+
+#include "core/object/class_db.h"
+
+void initialize_cryptokey_string_loader_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+		return;
+
+	ClassDB::bind_method(D_METHOD("load_from_string", "pem_string"), &CryptoKeyString::load_from_string);
+}
+
+void uninitialize_cryptokey_string_loader_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+		return;
+}
